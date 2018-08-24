bf8849770a4a52663cada842389917b365be50f8
kyle.zhou <kyle.zhou@bytedance.com>
2018-08-24 20:38:55 +0800

    更新版本

--- GJLiveEngineDemo/ZipArchive/ZipArchive.m
+++ GJLiveEngineDemo/ZipArchive/ZipArchive.m
@@ -133,7 +133,7 @@
 	{
 		data = [ NSData dataWithContentsOfFile:file];
 		uLong crcValue = crc32( 0L,NULL, 0L );
-		crcValue = crc32( crcValue, (const Bytef*)[data bytes], [data length] );
+		crcValue = crc32( crcValue, (const Bytef*)[data bytes], (uInt)[data length] );
 		ret = zipOpenNewFileInZip3( _zipFile,
 								  (const char*) [newname UTF8String],
 								  &zipInfo,
@@ -157,7 +157,7 @@
 	{
 		data = [ NSData dataWithContentsOfFile:file];
 	}
-	unsigned int dataLen = [data length];
+	unsigned int dataLen = (uInt)[data length];
 	ret = zipWriteInFileInZip( _zipFile, (const void*)[data bytes], dataLen);
 	if( ret!=Z_OK )
 	{
